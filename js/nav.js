1c52f34dd05357d2ab247be1dc2be749151166dd
juhee4839 <79627080+juhee4839@users.noreply.github.com>
2021-08-03 18:18:48 +0900

    finish
    finish

--- js/nav.js
+++ js/nav.js
@@ -1,2 +1,7 @@
 
-
+        $(document).ready(function(){
+            $("#trigger").click(function(){
+                $(".nav__bottom__gnb").toggleClass("on");
+                //클릭한 버튼에 change클래스를 추가 또는 제거, .side요소에 on클래스를 추가 또는 제거 
+            })
+        });
